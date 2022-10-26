ac3e7a919020e324b87e0baf1b9a2ee00fd4d8a7
SeekingL <1737320854@qq.com>
2022-10-26 12:54:08 +0800

    1

--- test.jl
+++ test.jl
@@ -1,27 +1,4 @@
-using Stipple
-
-@reactive mutable struct Name <: ReactiveModel
-  name::R{String} = "World!"
+function f(x)
+    readline()
 end
-
-function ui(model)
-  page( model, class="container", [
-      h1([
-        "Hello "
-        span("", @text(:name))
-      ])
-
-      p([
-        "What is your name? "
-        input("", placeholder="Type your name", @bind(:name))
-      ])
-    ]
-  )
-end
-
-route("/") do
-  model = Name |> init
-  html(ui(model), context = @__MODULE__)
-end
-
-up() # or `up(open_browser = true)` to automatically open a browser window/tab when launching the app
+println("result is ",f(5))
