f83863e1ce1bf7a2a2c520832047e3bcfc2ac03a
SeekingL <1737320854@qq.com>
2022-11-22 23:08:37 +0800

    小改一下格式

--- OurHeatlab/lib/ui.jl
+++ OurHeatlab/lib/ui.jl
@@ -5,7 +5,7 @@ function ui(model::MyApp.MyPage)
     #交互循环
     onany(model.value) do (_...)
         model.click[] += 1
-        if(sort(readdir(FILE_PATH))!=String[])
+        if (sort(readdir(FILE_PATH)) != String[])
             model.u0 = vec(float(open(readdlm, joinpath(FILE_PATH, "t1.txt"))))
         end
         change(model)
@@ -44,8 +44,11 @@ function ui(model::MyApp.MyPage)
         ),
         [
             row([
-                cell(class="st-module", size=8,[
-                h2("虚拟仿真平台：二维平板换热实验室")])
+                cell(class="st-module", size=8, [
+                    h1("&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp虚拟仿真平台：二维平板换热实验室
+                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp")])
                 cell(
                     class="st-module", size=3,
                     [
@@ -55,7 +58,7 @@ function ui(model::MyApp.MyPage)
                         #             style="offset: 10px 10px", "点击删除数据")
                         #     ]
                         # )
-                        uploader(label="数据上传", :auto__upload, :multiple, method="POST",
+                        uploader(label="初始温度上传", :auto__upload, :multiple, method="POST",
                             url=SERVEURL, field__name="txt_file")
                     ]
                 )
@@ -66,91 +69,91 @@ function ui(model::MyApp.MyPage)
                     class="st-module",
                     [
                         row([
-                        cell(
-                            class="st-module",
-                            [
-                                h5("Result Plot")
-                                plot(:plot_data, layout=:layout, config="{ displayLogo:false }")]
-                        )])
+                            cell(
+                                class="st-module",
+                                [
+                                    h5("Result Plot")
+                                    plot(:plot_data, layout=:layout, config="{ displayLogo:false }")]
+                            )])
                     ]
                 )
                 cell([
                     row([cell(
-                        class="st-module",size=10,
+                        class="st-module", size=10,
                         [
                             h2("&nbsp&nbsp&nbsp"),
                             h6("西边条件"),
-                            Stipple.select(:selection1, options=:selections,filled=true,rounded=true),
+                            Stipple.select(:selection1, options=:selections, filled=true, rounded=true),
                             input("", placeholder="对流换热系数h", @bind(:h1), @showif(:showinput1)),
                             input("", placeholder="关于t(时间)的表达式", @bind(:funcstr1))
                         ]
                     )])
                     row([
-                    cell(
-                        class="st-module",size=10,
-                        [
-                            h2("&nbsp&nbsp&nbsp"),
-                            h6("北边条件"),
-                            Stipple.select(:selection2, options=:selections,filled=true,rounded=true),
-                            input("", placeholder="对流换热系数h", @bind(:h2), @showif(:showinput2)),
-                            input("", placeholder="关于t(时间)的表达式", @bind(:funcstr2))
-                        ]
-                    )])
+                        cell(
+                            class="st-module", size=10,
+                            [
+                                h2("&nbsp&nbsp&nbsp"),
+                                h6("北边条件"),
+                                Stipple.select(:selection2, options=:selections, filled=true, rounded=true),
+                                input("", placeholder="对流换热系数h", @bind(:h2), @showif(:showinput2)),
+                                input("", placeholder="关于t(时间)的表达式", @bind(:funcstr2))
+                            ]
+                        )])
                     row([
-                    cell(
-                        class="st-module",size=10,
-                        [
-                            h2("&nbsp&nbsp&nbsp"),
-                            h6("东边条件"),
-                            Stipple.select(:selection3, options=:selections,filled=true,rounded=true),
-                            input("", placeholder="对流换热系数h", @bind(:h3), @showif(:showinput3)),
-                            input("", placeholder="关于t(时间)的表达式", @bind(:funcstr3))
-                        ]
-                    )])
+                        cell(
+                            class="st-module", size=10,
+                            [
+                                h2("&nbsp&nbsp&nbsp"),
+                                h6("东边条件"),
+                                Stipple.select(:selection3, options=:selections, filled=true, rounded=true),
+                                input("", placeholder="对流换热系数h", @bind(:h3), @showif(:showinput3)),
+                                input("", placeholder="关于t(时间)的表达式", @bind(:funcstr3))
+                            ]
+                        )])
                     row([
-                    cell(
-                        class="st-module",size=10,
-                        [
-                            h2("&nbsp&nbsp&nbsp"),
-                            h6("南边条件:"),
-                            Stipple.select(:selection4, options=:selections,filled=true,rounded=true),
-                            input("", placeholder="对流换热系数h", @bind(:h4), @showif(:showinput4)),
-                            input("", placeholder="关于t(时间)的表达式", @bind(:funcstr4))
-                        ]
-                    )])
+                        cell(
+                            class="st-module", size=10,
+                            [
+                                h2("&nbsp&nbsp&nbsp"),
+                                h6("南边条件:"),
+                                Stipple.select(:selection4, options=:selections, filled=true, rounded=true),
+                                input("", placeholder="对流换热系数h", @bind(:h4), @showif(:showinput4)),
+                                input("", placeholder="关于t(时间)的表达式", @bind(:funcstr4))
+                            ]
+                        )])
                     row([
-                    cell(
-                        class="st-module",size=10,
-                        [
-                            h2("&nbsp&nbsp&nbsp"),
-                            h6("内热源: &nbsp&nbsp"),
-                            input("", placeholder="关于t(时间)的表达式", @bind(:innerheat))
-                        ]
-                    )])
+                        cell(
+                            class="st-module", size=10,
+                            [
+                                h2("&nbsp&nbsp&nbsp"),
+                                h6("内热源: &nbsp&nbsp"),
+                                input("", placeholder="关于t(时间)的表达式", @bind(:innerheat))
+                            ]
+                        )])
                     row([
-                    cell(
-                        size=8,
-                        [
-                            btn("Simulation!", color="primary", textcolor="black", @click("value += 1"),
-                                [
-                                    tooltip(contentclass="bg-indigo", contentstyle="font-size: 16px",
-                                        style="offset: 1000px 1000px", "Click the button to start simulation")
-                                    h6(["&nbsp&nbspTimes: ",
-                                        span(model.click, @text(:click))])
-                                ]
-                            )
-                        ]
-                    )])
+                        cell(
+                            size=8,
+                            [
+                                btn("Simulation!", color="primary", textcolor="black", @click("value += 1"),
+                                    [
+                                        tooltip(contentclass="bg-indigo", contentstyle="font-size: 16px",
+                                            style="offset: 1000px 1000px", "Click the button to start simulation")
+                                        h6(["&nbsp&nbspTimes: ",
+                                            span(model.click, @text(:click))])
+                                    ]
+                                )
+                            ]
+                        )])
                 ])
             ])
             row([
-                    cell(
-                        class="st-module",
-                        [
-                            h5("Result Data")
-                            table(:tableData; pagination=:credit_data_pagination, label=false, flat=true)
-                        ]
-                    )
+                cell(
+                    class="st-module",
+                    [
+                        h5("Result Data")
+                        table(:tableData; pagination=:credit_data_pagination, label=false, flat=true)
+                    ]
+                )
             ])
         ]
     )
